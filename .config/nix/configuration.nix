00a6b7f11ed1fe8a2fced7ca8d0af746b04a67d8
dhdb19 <jordi.pagr@gmailcom>
2025-04-30 23:06:21 +0200

    First commit of all config giles

---
+++ .config/nix/configuration.nix
@@ -0,0 +1,427 @@
+# Edit this configuration file to define what should be installed on
+# your system. Help is available in the configuration.nix(5) man page, on
+# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
+
+{ config, lib, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      ./cachix.nix
+    ];
+
+
+  # Use latest kernel
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "nixos"; # Define your hostname.
+  # Pick only one of the below networking options.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+
+  # Set your time zone.
+  time.timeZone = "Europe/Berlin";
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "de_DE.UTF-8";
+    LC_PAPER = "en_GB.UTF-8";
+    LC_TIME = "en_GB.UTF-8";
+  };
+  console = {
+  earlySetup = true;
+  #    font = "Lat2-Terminus16";
+  #  font = "latarcyrheb-sun32";
+  #  font = "sun12x22";
+  # font = "iso01-12x22";
+  font = "ter-i24b";
+  packages = with pkgs; [
+      terminus_font
+    ];
+ #    keyMap = "de";
+    useXkbConfig = true; # use xkb.options in tty.
+  };
+
+  # Enable the X11 windowing system.
+  # services.xserver.enable = true;
+  services.xserver = {
+    enable = false;
+  };
+
+  # enable qmk
+  hardware.keyboard.qmk.enable = true;
+  services.udev.packages = [ pkgs.via ];
+  
+  # Enable OpenGl
+  hardware.graphics = {
+    enable = true;
+    extraPackages = with pkgs; [
+      vaapiVdpau
+      libvdpau-va-gl
+      nvidia-vaapi-driver
+    ];
+  };
+
+  # Load nvidia driver for Xorg and Wayland
+  hardware.nvidia = {
+    # Modesetting is required
+    modesetting.enable = true;
+    # experimental and unstable
+    powerManagement.enable = false;
+    powerManagement.finegrained = false;
+    # use open-source driver
+    open = false;
+    #enable nvidia settings
+    nvidiaSettings = true;
+    # choose driver package
+    package = config.boot.kernelPackages.nvidiaPackages.latest;
+  };
+  # Configure keymap in X11
+  services.xserver.xkb.layout = "de";
+  # services.xserver.xkb.options = "eurosign:e,caps:escape";
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  # Enable sound.
+  # hardware.pulseaudio.enable = true;
+  # OR
+  services.pipewire = {
+     enable = true;
+     pulse.enable = true;
+   };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.jordi = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+    packages = with pkgs; [
+      tree
+    ];
+  };
+
+  
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  nixpkgs = {
+    config = {
+      allowUnfree = true;
+      packageOverrides = pkgs: {
+       unstable = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz") {};
+      };
+    };
+  };
+
+
+
+  environment.systemPackages = with pkgs; 
+  let
+    swayConfig = pkgs.writeText "greetd-sway-config" ''
+    # -l activates layer-shell mode. Notice that swaymsg will run ater gtkgreet
+    exec "${pkgs.greetd.tuigreet}/bin/tuigreet -l; swaymsg exit"
+    bindsym Mod4+shift+e exec swaynag\
+      -t warning \
+      -m "What do you want to do?" \
+      -b 'Poweroff' 'systemctl poweroff' \
+      -b 'Reboot' 'systemctl reboot'
+    '';
+    R_custom = rWrapper.override{
+      packages = with unstable.rPackages; [
+        tidyverse
+        #duckdb
+        DBI
+        duckplyr
+        ggeffects
+        gh
+        gitcreds
+        glue
+        gt
+        gridExtra
+        haven
+        googledrive
+        httpgd
+        knitr
+        MASS
+        tikzDevice
+        tinytable
+        viridis
+        vroom
+        parallel
+        rix    
+      ];
+    };
+  in 
+   [
+    R_custom
+    vim
+    wget
+    neovim
+    firefox
+    labwc
+    swayfx
+    swaybg
+    greetd.greetd
+    greetd.tuigreet
+    R
+    python314
+    yazi
+    wl-clipboard
+    mako
+    fastfetch
+    zoxide
+    stow
+    htop
+    btop
+    zenith
+    pciutils
+    waybar
+    wofi
+    zinit
+    git-credential-manager
+    pass-wayland
+    gnupg
+    pinentry-tty
+    fzf
+    helix
+    ffmpeg
+    jq
+    poppler
+    fd
+    resvg
+    ripgrep
+    imagemagick
+    vscodium
+    zed-editor
+    keepassxc
+    onedriver
+    unstable.onedrive
+    pydf
+    libgcc
+    libgccjit
+    postgresql
+    cachix
+    unzip
+    syncthing
+    bat
+    eza
+    lsd
+    powertop
+    wlr-randr
+    dconf
+    kitty
+    egl-wayland
+    nvidia-vaapi-driver
+    via
+    starship
+    eza
+    lsd
+    git-credential-keepassxc
+    sioyek
+  ];
+
+  programs.tmux = {
+    enable = true;
+    newSession = true;
+  };
+
+  xdg.portal = {
+    enable = true;
+    wlr.enable = true;
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-wlr
+    ];
+  };
+
+
+
+  environment.etc."greetd/environments".text = ''
+  labwc
+  sway
+  hyprland
+  '';
+  
+  programs.hyprland = {
+    enable = true;
+    xwayland .enable = true;
+    withUWSM = true;
+    portalPackage = pkgs.xdg-desktop-portal-hyprland;
+  };
+
+  programs.hyprlock = {
+    enable = true;
+  };
+
+  programs.uwsm = {
+    enable = true;
+  };
+  
+   
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
+
+  programs.sway = {
+    enable = true;
+    wrapperFeatures.gtk = true;
+  };
+
+  programs.git = {
+    enable = true;
+    lfs.enable = true;
+  };
+
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    syntaxHighlighting = {
+      enable = true;
+      highlighters = [
+        "main"
+      ];
+    };
+    autosuggestions = {
+      enable = true;
+      async = true;
+    };
+    vteIntegration = true;
+    enableGlobalCompInit = true;
+  };
+
+  # users.defaultUserShell = pkgs.zsh;
+
+  environment.pathsToLink = [
+    "share/foot"
+  ];
+
+  programs.foot = {
+    enable = true;
+    # enableZshIntegration = true;
+    theme = "catpuccin-mocha";
+  };
+
+  #Fonts
+  fonts = {
+    enableDefaultPackages = true;
+    packages = with pkgs; [
+      nerdfonts
+    ];
+
+    fontconfig = {
+      defaultFonts = {
+        serif = [ "Liberation Serif" ];
+	sansSerif = [ "BitStromWerat" ];
+	monospace = [ "JetBrainsMono" ];
+      };
+    };
+  };
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  services.gnome.gnome-keyring.enable = true;
+
+  
+  services.pcscd.enable = true;
+
+  services.onedrive = {
+    enable = true;
+  };
+
+
+services.syncthing = {
+    enable = true;
+    group = "users";
+    user = "jordi";
+    dataDir = "${config.users.users.jordi.home}/sync";
+    configDir = "${config.users.users.jordi.home}/.config/syncthing";
+    overrideDevices = true;
+    overrideFolders = true;
+    openDefaultPorts = true;
+    settings = {
+      devices = {
+        "oneplus" = { id = "QNHUVLY-D5GTXSA-BBC3XTZ-CQTPY2Y-UK3HXWK-N62NOJC-4PJJTFH-WDE3DQ6"; };
+      };
+      # folders = {
+      # "sync" = {
+      #   path = "~/sync";
+      #   devices = [ "oneplus" ];
+      #   };
+      # };
+    };
+  };
+
+  services.hypridle = {
+    enable = true;
+  };
+
+  security.polkit = {
+    enable = true;
+  };
+  
+  services.greetd = {
+    enable = true;
+    settings = rec {
+      initial_session = {
+      command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --greeting 'Hello there!'  --remember --width 60 --greet-align left --asterisks --cmd labwc";
+      user = "jordi";
+      };
+      default_session = initial_session;
+    };
+  };
+
+    # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # Copy the NixOS configuration file and link it from the resulting system
+  # (/run/current-system/configuration.nix). This is useful in case you
+  # accidentally delete configuration.nix.
+  # system.copySystemConfiguration = true;
+
+  # This option defines the first version of NixOS you have installed on this particular machine,
+  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
+  #
+  # Most users should NEVER change this value after the initial install, for any reason,
+  # even if you've upgraded your system to a new NixOS release.
+  #
+  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
+  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
+  # to actually do that.
+  #
+  # This value being lower than the current NixOS release does NOT mean your system is
+  # out of date, out of support, or vulnerable.
+  #
+  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
+  # and migrated your data accordingly.
+  #
+  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
+  system.stateVersion = "24.11"; # Did you read the comment?
+
+}
+
--- .zshrc
+++ .zshrc
@@ -1,12 +1,88 @@
+#zsh-hook
+autoload -U add-zsh-hook
+
+#Package Manager
+# ZINIT_HOME="${XDG_DATA_HOME:-${HOME}/.local/share}/zinit/zinit.git"
+
+# Download zinit if it's not there
+# if [ ! -d "$ZINIT_HOME" ]; then
+# 	mkdir -p "$(dirname $ZINIT_HOME)"
+# 	git clone https://github.com/zdharma-continuum/zinit.git "$ZINIT_HOME"
+# fi
+
+# Source/load zinit
+# source "${ZINIT_HOME}/zinit.zsh"
+
+#Plugin syntax highlighting the important ones
+# zinit light zsh-users/zsh-syntax-highlighting
+# zinit light zsh-users/zsh-completions
+# zinit light zsh-users/zsh-autosuggestions
+# zinit light Aloxaf/fzf-tab
+
+#Add snippets
+# zinit snippet OMZP::git
+
+#Shell integrations
+tmux has-session -t development
+if [ $? != 0 ]
+then
+  tmux new-session -s development
+fi
+tmux attach -t development
+fastfetch
+eval "$(fzf --zsh)"
+eval "$(zoxide init --cmd cd zsh)"
+eval "$(starship init zsh)"
+
+#Aliases
+alias ls='ls --color'
+alias ll='ls -la'
+alias nixconfig='sudo hx /etc/nixos/configuration.nix'
+alias nixrebuild='sudo nixos-rebuild switch'
+alias swayconfig='hx ~/.config/sway/config'
+alias labenv='hx ~/.config/labwc/environment'
+alias labmenu='hx ~/.config/labwc/menu.xml'
+alias labrc='hx ~/.config/labwc/rc.xml'
+alias waybarconfig='hx ~/.config/waybar/config'
+alias waybarcss='hx ~/.config/waybar/style.css'
+alias zshconfig='hx ~/.zshrc'
+alias fzf='fzf -m --preview="bat --color=always {} "'
+alias hxfzf='hx $(fzf)'
+alias nixupdate='sudo nixos-rebuild switch --upgrade'
+
+# Load completions
+autoload -Uz compinit && compinit
+# zinit cdreplay -q
+
+# fzf-tab
+source ~/.config/zsh/fzf-tab/fzf-tab.plugin.zsh
+
+#Completion styling
+zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
+zstyle ':completion:*' list-colors "${(s.:.)LS_COLORS}"
+zstyle ':completion:*' menu no
+zstyle ':fzf-tab:complete:cd:*' fzf-preview 'ls --color $realpath'
+zstyle ':fzf-tab:complete:__zoxide_z:*' fzf-preview 'ls --color $realpath'
+
+#configure autosggestions
+bindkey '^f' autosuggest-accept
+
 # Lines configured by zsh-newuser-install
-HISTFILE=~/.histfile
-HISTSIZE=1000
-SAVEHIST=1000
+HISTFILE=~/.config/zsh/.zshhist
+HISTSIZE=5000
+SAVEHIST=$HISTSIZE
+HISTDUP=erase
+setopt appendhistory
+setopt sharehistory
+setopt hist_ignore_space
+setopt hist_ignore_all_dups
+setopt hist_save_no_dups
+setopt hist_ignore_dups
+setopt hist_find_no_dups
 bindkey -v
 # End of lines configured by zsh-newuser-install
 # The following lines were added by compinstall
 zstyle :compinstall filename '/home/jordi/.zshrc'
 
-autoload -Uz compinit
-compinit
+
 # End of lines added by compinstall
